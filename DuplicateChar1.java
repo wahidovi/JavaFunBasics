987984a502e1a4764fdb91d073360d1ed80535ac
wahidovi <md.wahidovi@gmail.com>
2022-06-28 11:46:10 -0700

    fixed comment

--- DuplicateChar1.java
+++ DuplicateChar1.java
@@ -35,7 +35,7 @@ public class DuplicateChar1 {
 						k++; // k count the occurrence. how many of same char we have
 						num = k;
 						d = a.charAt(j); // d holds the current char we checked
-										// it helps the above to avoid counting 
+										// it helps the above if to avoid counting 
 										// for same char multiple times
 					}
 				}
